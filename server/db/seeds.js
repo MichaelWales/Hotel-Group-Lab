e81a4cbb4d764af51a8772c77f8becb42101f9bc
Teo <43215755+teokigi@users.noreply.github.com>
2020-05-05 15:02:02 +0100

    2 seed files

--- server/db/seeds.js
+++ server/db/seeds.js
@@ -3,6 +3,5 @@ db.dropDatabase();
 
 db.bookings.insertMany([
     { "customer":"Adolf Mitler", "emailAddress":"AdolfMitler@hotmail.com" , "checkedIn":false},
-    { }
-
-])
+    { "customer":"Tony Hawk", "emailAddress":"ProSkater@hotmail.com", "checkedIn":true}
+]);
