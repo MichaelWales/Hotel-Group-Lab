d07714e46c669aa119b6bae42d61e1616cccc3f9
Christopher Pearson <cpearson3140@gmail.com>
2020-05-05 19:26:37 +0100

    First Front End commit

---
+++ client/src/components/HolidayBookingForm.vue
@@ -0,0 +1,20 @@
+<template lang="html">
+  <form id="sightings-form" v-on:submit.prevent="handleSubmit">
+    <h2>Add a Booking</h2>
+    <div class="formWrap">
+      <label for="name">Name:</label>
+      <input type="text" id="name" v-model="name" />
+    </div>
+    <div class="formWrap">
+      <label for="email address">Email Address:</label>
+      <input type="text" id="email address" v-model="email address" />
+    </div>
+     <li :class="guest.checked_in ? 'Checked In' : 'Not Checked in'" v-for="checked_in in holidayBookingForm">
+     <button v-if="!guest.checked_in" v-on:click="updateList(guest)">Check In!</button>
+</template>
+<script>
+export default {
+}
+</script>
+<style lang="css" scoped>
+</style>
---
+++ client/src/components/HolidayBookingList.vue
@@ -0,0 +1,14 @@
+<template lang="html">
+    <div id="selected_customer">
+        hello World
+        <!-- <h2> {{selectedCustomer.name}} </h2> -->
+    </div>
+</template>
+<script>
+export default {
+  name: 'customer-detail',
+  props: ['customersInBookingList']
+}
+</script>
+<style lang="css" scoped>
+</style>
