6302a71046dc6c4d4c1e5c5122e72b2e67ab27eb
Teo <43215755+teokigi@users.noreply.github.com>
2020-05-05 16:37:40 +0100

    10seeds

--- server/db/seeds.js
+++ server/db/seeds.js
@@ -3,5 +3,13 @@ db.dropDatabase();
 
 db.bookings.insertMany([
     { "customer":"Adolf Mitler", "emailAddress":"AdolfMitler@hotmail.com" , "checkedIn":false},
-    { "customer":"Tony Hawk", "emailAddress":"ProSkater@hotmail.com", "checkedIn":true}
+    { "customer":"Tony Hawk", "emailAddress":"ProSkater@hotmail.com", "checkedIn":true},
+    { "customer":"Kim Jong Un", "emailAddress":"amNotDead@hotmail.com","checkedIn":false},
+    { "customer":"Boris Johnson", "emailAddress":"givingGuestsCovid@hotmail.com", "checkedIn":true},
+    { "customer":"Justin Trudeau", "emailAddress":"amATeacher@hotmail.com","checkedIn":false},
+    { "customer":"Anne Frank", "emailAddress": "NinjaInYourAttic@hotmail.com","checkedIn":true},
+    { "customer":"Jack Churchill", "emailAddress":"BringingSword2GunFight@hotmail.com","checkedIn":true},
+    { "customer":"Alfred Yankovic", "emailAddress":"WeirdAl@hotmail.com","checkedIn":false},
+    { "customer":"Nobuo Uematsu", "emailAddress":"FinalFantasyMusician@hotmail.com","checkedIn":false},
+    { "customer":"Oda Nobunaga", "emailAddress":"SengokuPeriod@hotmail.com","checkedIn":true}
 ]);
