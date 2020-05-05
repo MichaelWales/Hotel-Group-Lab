5215101d0d639941e56f639ebeb19fd1a823bd09
Michael Wales <michaelwales@gmail.com>
2020-05-05 15:44:01 +0100

    Adds server.js setup

--- server/server.js
+++ server/server.js
@@ -1,1 +1,15 @@
-Blah Blah Blah
+const express = require('express');
+const app = express();
+const MongoClient = require('mongodb').MongoClient;
+const createRouter = require('./helpers/create_router.js');
+const cors = require('cors');
+const parser = require('body-parser');
+
+app.use(parser.json());
+app.use(cors());
+
+
+
+app.listen(3000, function() {
+  console.log('Listening on port 3000');
+});
