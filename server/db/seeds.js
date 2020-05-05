1eb1679891f11479df251e70611b20630d5c8e57
Teo <43215755+teokigi@users.noreply.github.com>
2020-05-05 15:48:28 +0100

    seeds

--- server/db/seeds.js
+++ server/db/seeds.js
@@ -3,5 +3,9 @@ db.dropDatabase();
 
 db.bookings.insertMany([
     { "customer":"Adolf Mitler", "emailAddress":"AdolfMitler@hotmail.com" , "checkedIn":false},
+<<<<<<< Updated upstream
     { "customer":"Tony Hawk", "emailAddress":"ProSkater@hotmail.com", "checkedIn":true}
+=======
+    { "customer":"Tony Hawk", "emailAddress":"ProSkater@hotmail.com", "checkedIn":true},
+>>>>>>> Stashed changes
 ]);
