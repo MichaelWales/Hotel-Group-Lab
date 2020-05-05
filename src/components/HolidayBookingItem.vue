8cbdd9d5352e2add35417c8272a19832671e5122
Christopher Pearson <cpearson3140@gmail.com>
2020-05-05 16:28:30 +0100

    Updated Components

---
+++ src/components/HolidayBookingItem.vue
@@ -0,0 +1,32 @@
+<template lang="html">
+  <div class="">
+    <label for="customer_select">Select a Customer:</label>
+    <select id="customer_select" v-on:charge="customerSelected"model="selectedCustomer">
+      <option disabled value="">Select a customer</option>
+      <option v-for="customer in customers":value="customer">{{customer.name}}</option>
+    </select>
+  </div>
+</template>
+
+<script>
+
+import {eventBus} from '@/main.js'
+
+export default {
+  name: 'customer-select',
+  props: ['customers'],
+  data() {
+    return {
+      selectedCustomer: null
+    }
+  },
+  methods: {
+    countrySelected(){
+      eventBus.$emit('customer-selected', this.selectedCustomer)
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
---
+++ src/components/HolidayBookingList.vue
@@ -0,0 +1,14 @@
+<template lang="html">
+  <div id="selected_customer">
+    <h2> {{selectedCustomer.name}} </h2>
+</template>
+
+<script>
+export default {
+  name: 'customer-detail',
+  props: ['selectedCustomer']
+}
+</script>
+
+<style lang="css" scoped>
+</style>
